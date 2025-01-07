239db79d3b887f803761ca4f2f40148bb585c792
gaelfelix <gael.felix@icloud.com>
2025-01-07 11:52:15 +0100

    fix:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,44 @@
+/// <reference types="vite-plugin-svgr/client" />
+
 import "../assets/styles/scss/components/_Header.scss";
+import HomeIcon from "../assets/images/icons/Home.svg?react";
+import RadarIcon from "../assets/images/icons/Radar.svg?react";
+import SearchIcon from "../assets/images/icons/Search.svg?react";
+import SlayderIcon from "../assets/images/icons/Add.svg?react";
+import NotificationIcon from "../assets/images/icons/Notif.svg?react";
+import ProfileIcon from "../assets/images/icons/Profile.svg?react";
 
 function Header() {
-return (
+    return (
         <header className="page-header">
-            <p>header</p>
+            <nav>
+                <ul>
+                <li>
+                    <HomeIcon />
+                    <p>Accueil</p>
+                </li>
+                <li>
+                    <RadarIcon />
+                    <p>Radar</p>
+                </li>
+                <li>
+                    <SearchIcon />
+                    <p>Recherche</p>
+                </li>
+                <li>
+                    <SlayderIcon />
+                    <p>Slayder</p>
+                </li>
+                <li>
+                    <NotificationIcon />
+                    <p>Notifs</p>
+                </li>
+                <li>
+                    <ProfileIcon />
+                    <p>Profil</p>
+                </li>
+                </ul>
+            </nav>
         </header>
     )
 }
