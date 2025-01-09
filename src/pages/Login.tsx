86420d2f695cbab3c8b71e1c07f38b2c6c824d10
Marion1b <121030423+Marion1b@users.noreply.github.com>
2025-01-09 14:50:21 +0100

    Feature/sign in form (#6)
    * feat: add links in nav to connect to pages
    
    * feat : sign in form html
    
    * feat :  center align login form
    
    * feat : sign-in form style
    
    * feat : add paragraph and link to register page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,23 @@
+import "../assets/styles/scss/pages/_Login.scss";
+
 function Login() {
     return (
-        <>
-        <h1>CONNEXION</h1>
-        </>
+        <main className="login-page">
+            <h1>Connexion</h1>
+            <form action="" method="post">
+                <div className="login-page-fieldset">
+                    <label htmlFor="email">Adresse mail : </label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="login-page-fieldset">
+                    <label htmlFor="password">Mot de passe : </label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Se connecter</button>
+                <p>Pas encore de compte ? <a href="./inscription">S'inscrire</a></p>
+            </form>
+        </main>
     )
 }
-  
+
 export default Login
