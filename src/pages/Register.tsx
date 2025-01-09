7eb71049a4707086a3e4f0f6e7a7415e8616d5e8
Marion1b <marion.barthoux@gmail.com>
2025-01-09 15:04:59 +0100

    feat: html form sign up

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,28 @@
 function Register() {
     return (
-        <>
-        <h1>INSCRIPTION</h1>
-        </>
+        <main className="register-page">
+            <h1>Inscription</h1>
+            <form action="" method="post">
+                <div className="register-page-fieldset">
+                    <label htmlFor="email">Adresse mail : </label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="username-page-fieldset">
+                    <label htmlFor="username">Pseudo : </label>
+                    <input type="text" id="username" name="username" required />
+                </div>
+                <div className="register-page-fieldset">
+                    <label htmlFor="password">Mot de passe : </label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <div className="register-page-fieldset">
+                    <label htmlFor="confirm-password">Confirmer le mot de passe : </label>
+                    <input type="password" id="confirm-password" name="confirm-password" required />
+                </div>
+                <button type="submit">Se connecter</button>
+                <p>Pas encore de compte ? <a href="./inscription">S'inscrire</a></p>
+            </form>
+        </main>
     )
 }
   
