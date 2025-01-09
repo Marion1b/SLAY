fd2ac35c556b9113fc79ebb96db47fa59f7eee02
Marion1b <marion.barthoux@gmail.com>
2025-01-09 16:01:36 +0100

    feat: start function to check password

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,24 @@
 import "../assets/styles/scss/pages/_Register.scss";
+import {useState} from 'react';
+import {isSamePassword, isPasswordCorrect} from "../utils/authUtils.ts";
 
 function Register() {
+    const [password1, setPassword1]=useState<string>("");
+    const [password2, setPassword2]=useState<string>("");
+
+    function assignPasswords(setPassword:React.Dispatch<React.SetStateAction<string>>,e : React.ChangeEvent<HTMLInputElement>):void{
+        setPassword(e.target.value.toString());
+    }
+
+    function checkPasswords(e : React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        console.log(password1 + " ???? " + password2);
+    }
+
     return (
         <main className="register-page">
             <h1>Inscription</h1>
-            <form action="" method="post">
+            <form action="" method="post" onSubmit={checkPasswords}>
                 <div className="register-page-fieldset">
                     <label htmlFor="email">Adresse mail : </label>
                     <input type="email" id="email" name="email" required />
@@ -15,11 +29,11 @@ function Register() {
                 </div>
                 <div className="register-page-fieldset">
                     <label htmlFor="password">Mot de passe : </label>
-                    <input type="password" id="password" name="password" required />
+                    <input type="password" id="password" name="password" onChange={(e) => assignPasswords(setPassword1, e)} required />
                 </div>
                 <div className="register-page-fieldset">
                     <label htmlFor="confirm-password">Confirmer le mot de passe : </label>
-                    <input type="password" id="confirm-password" name="confirm-password" required />
+                    <input type="password" id="confirm-password" name="confirm-password" onChange={(e) => assignPasswords(setPassword2, e)} required />
                 </div>
                 <button type="submit">Se connecter</button>
                 <p>Déjà un compte ? <a href="./connexion">Se connecter</a></p>
@@ -27,5 +41,5 @@ function Register() {
         </main>
     )
 }
-  
+
 export default Register
