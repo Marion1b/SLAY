5f4c63d324f4eaef5b22a65d4d4a2ec0d5aaf92c
Marion1b <marion.barthoux@gmail.com>
2025-01-09 11:11:30 +0100

    feat : sign in form html

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,20 @@
 function Login() {
     return (
-        <>
-        <h1>CONNEXION</h1>
-        </>
+        <main className="login-page">
+            <h1>CONNEXION</h1>
+            <form action="" method="post">
+                <div className="login-page-fieldset">
+                    <label htmlFor="email">Adresse mail : </label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="login-page-fieldset">
+                    <label htmlFor="password">Mot de passe : </label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Se connecter</button>
+            </form>
+        </main>
     )
 }
-  
+
 export default Login
