bd45c9780a773008ce3aeaafd10ba60196b268b2
gaelfelix <gael.felix@icloud.com>
2025-01-10 11:30:15 +0100

    feat: landing page ok

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,15 @@
 import "../assets/styles/scss/components/_LandingPage.scss";
+import ChevronDown from "../assets/images/icons/ChevronDown.svg?react";
 
 function LandingPage() {
     return (
-        <section>
-            
+        <section className="landing-page" >
+            <h1>SLAY</h1>
+            <p className="subtitle">Partagez vos créations musicales, découvrez de nouveaux sons et rencontrez d'autres artistes.</p>
+            <div>
+                <p>Découvrir</p>
+                <ChevronDown />
+            </div>
         </section>
     )
 }
