2b03ceb4248e697a36cabf2253c9554d2d6cf9bd
gaelfelix <gael.felix@icloud.com>
2025-01-10 09:41:26 +0100

    feat: create landing page file

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,11 @@
+import "../assets/styles/scss/components/_LandingPage.scss";
+
+function LandingPage() {
+    return (
+        <section>
+            
+        </section>
+    )
+}
+
+export default LandingPage
