e98a0476f5a3c50077d32200690e2a42aacd291f
Marion1b <121030423+Marion1b@users.noreply.github.com>
2025-01-13 14:56:38 +0100

    Feature/slay button (#9)
    * feat: create react button and start styling it
    
    * feat : end style button slay
    
    * feat : interactive button (change slay unslay on click)
    
    * feat : remove console.log

---
+++ src/components/SlayButton.tsx
@@ -0,0 +1,31 @@
+import UserPlus from "../assets/images/icons/UserPlus.svg?react";
+import UserRemove from "../assets/images/icons/UserRemove.svg?react";
+import "../assets/styles/scss/components/_SlayButton.scss";
+import { useState, useEffect } from "react";
+
+interface WrapperProps{
+    slay: boolean
+}
+
+function SlayButton(props: WrapperProps){
+    const [slay, setSlay] = useState<boolean>(props.slay);
+    useEffect(() => {
+        setSlay(props.slay);
+    }, [props.slay]);
+
+    if(slay){
+        return(
+            <button className="slay-button slay-button-remove" onClick={() =>setSlay(!slay)}>
+                Unslay <UserRemove />
+            </button>
+        )
+    }else{
+        return(
+            <button className="slay-button slay-button-plus" onClick={() =>setSlay(!slay)}>
+                Slay <UserPlus />
+            </button>
+        )
+    }
+}
+
+export default SlayButton;
