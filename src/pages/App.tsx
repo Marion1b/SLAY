c81158cdef2d71c1ca5d2ba63ea0f30d7e34667a
gaelfelix <gael.felix@icloud.com>
2025-01-13 15:24:26 +0100

    fix: delete test composant on app.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,9 @@
 import LandingPage from "../components/LandingPage";
-import ProfilePictureAndInfo from "../components/ProfilePictureAndInfo";
 
 function App() {
   return (
     <main className="home-page">
       <LandingPage />
-      <ProfilePictureAndInfo />
     </main>
   )
 }
