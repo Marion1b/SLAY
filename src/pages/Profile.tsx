2405021d3db4b85ae87fa63af9bcf05e68c72a74
Marion1b <marion.barthoux@gmail.com>
2025-01-13 15:52:54 +0100

    feat: profile infos (reposts, slayers and slay) react and style

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,14 @@
 import SlayButton from "../components/SlayButton";
+import NumbersInfos from "../components/NumbersInfos";
 
 function Profile() {
 
     return (
-        <>
+        <section className="profile-page">
             <h1>Profil</h1>
             <SlayButton slay={false} />
-        </>
+            <NumbersInfos reposts={4500} slayers={3200} suivis={439} />
+        </section>
     )
 }
 export default Profile
