0e709aa73215404f19f2010fdc6cb32a8db65113
gaelfelix <gael.felix@icloud.com>
2025-01-14 10:40:35 +0100

    feat: profilepictureandinfos props

--- src/components/ProfilePictureAndInfo.tsx
+++ src/components/ProfilePictureAndInfo.tsx
@@ -1,12 +1,19 @@
 import "../assets/styles/scss/components/_ProfilePictureAndInfo.scss";
 
-function ProfilePictureAndInfo() {
+interface WrapperProps{
+    name: string;
+    city: string;
+    country: string;
+    pronouns: string;
+}
+
+function ProfilePictureAndInfo(props: WrapperProps) {
     return (
         <section className="profile-picture-and-info" >
             <img src="./src/assets/images/test/Antoine_Daniel.jpg" alt="Photo de profil de Antoine Daniel" />
-            <p className="profile-name">Antoine Daniel</p>
-            <p className="profile-location">Montpellier, France</p>
-            <p className="profile-pronouns">il</p>
+            <p className="profile-name">{props.name}</p>
+            <p className="profile-location">{props.city}, {props.country}</p>
+            <p className="profile-pronouns">{props.pronouns}</p>
         </section>
     )
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,13 @@
 import SlayButton from "../components/SlayButton";
 import NumbersInfos from "../components/NumbersInfos";
+import ProfilePictureAndInfo from "../components/ProfilePictureAndInfo";
 
 function Profile() {
 
     return (
         <section className="profile-page">
             <h1>Profil</h1>
+            <ProfilePictureAndInfo name="Antoine Daniel" city="Montpellier" country="France" pronouns="il" />
             <SlayButton slay={false} />
             <NumbersInfos reposts={4500} slayers={3200} suivis={439} />
         </section>
