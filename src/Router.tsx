1c4cda7188c7dfb520c0eabc58bffba5d09437b6
Marion1b <marion.barthoux@gmail.com>
2025-02-03 14:30:34 +0100

    feat : secure root to profile page

---
+++ src/Router.tsx
@@ -0,0 +1,62 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+// Pages
+import App from './pages/App.tsx';
+import Login from './pages/Login.tsx';
+import ModifyProfile from './pages/ModifyProfile.tsx';
+import Profile from './pages/Profile.tsx';
+import Radar from './pages/Radar.tsx';
+import Register from './pages/Register.tsx';
+import Search from './pages/Search.tsx';
+import Slayder from './pages/Slayder.tsx';
+import ProtectedRoute from './components/ProtectedRoute.tsx';
+
+const getAccessToken= () =>{
+    return Cookies.get('accessToken');
+}
+
+const isAuthenticated = () => {
+    return !!getAccessToken();
+}
+
+const Router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <App />,
+            index:true
+        },
+        {
+            path: "/connexion",
+            element: <Login />
+        },
+        {
+            path:"/inscription",
+            element: <Register />
+        },
+        {
+            element: <ProtectedRoute isAuthenticated={isAuthenticated()} />,
+            children: [
+                {
+                    path: '/modifier-mon-profil',
+                    element:<ModifyProfile />
+                },
+                {
+                    path: '/profil',
+                    element: <Profile />
+                },
+                {
+                    path: 'radar',
+                    element: <Radar />
+                }
+            ]
+        },
+        {
+            path:"*",
+            element: <p>404 Error - Nothing here...</p>
+        }
+    ]
+)
+
+export default Router;
