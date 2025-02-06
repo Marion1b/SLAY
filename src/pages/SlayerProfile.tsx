2b81295971ee783691e9cecf24ba3775f7e4e7ab
Marion1b <121030423+Marion1b@users.noreply.github.com>
2025-02-06 11:11:00 +0100

    Feature/personnal profile page (#19)
    * feature: change adapt profile page for connected user
    
    * feat: personnal profile page

--- src/pages/SlayerProfile.tsx
+++ src/pages/SlayerProfile.tsx
@@ -1,11 +1,10 @@
 import "../assets/styles/scss/pages/_Profile.scss";
+import SlayButton from "../components/SlayButton";
 import ProfilePictureAndInfo from "../components/ProfilePictureAndInfo";
 import NumbersInfos from "../components/NumbersInfos";
 import ProfileBiography from "../components/ProfileBiography";
-import ModifyProfileButton from "../components/ModifyProfileButton";
-import { Link } from 'react-router-dom';
 
-//Profile page for connected user
+//Profile page of another user
 
 function Profile() {
 
@@ -13,14 +12,12 @@ function Profile() {
         <section className="profile-page">
             <h1 hidden>Profil</h1>
             <div className="profile-section-left">
-                <ProfilePictureAndInfo avatar={sessionStorage.getItem('slayerAvatar')} name={sessionStorage.getItem('slayerPseudo')} city="Montpellier" country="France" pronouns={sessionStorage.getItem('slayerPronouns')} />
+                <ProfilePictureAndInfo name="Antoine Daniel" city="Montpellier" country="France" pronouns="il" />
             </div>
             <div className="profile-section-right">
                 <NumbersInfos reposts={4500} slayers={3200} suivis={439} />
                 <ProfileBiography className="profile-biography-profile"/>
-                <Link to="/modifier-mon-profil">
-                    <ModifyProfileButton />
-                </Link>
+                <SlayButton slay={false} />
             </div>
         </section>
     )
